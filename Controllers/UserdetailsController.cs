a040ea6a40990c75bce33fc1f3d179a6bb994280
Sushmanth Natha <sushmanthnatha@gmail.com>
2021-04-27 20:23:28 +0530

    initial commit

---
+++ Controllers/UserdetailsController.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using CravingAPI.Models;
+using Microsoft.AspNetCore.Cors;
+
+namespace CravingAPI.Controllers
+{
+    
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserdetailsController : ControllerBase
+    {
+        private readonly cravingContext _context;
+        
+        public UserdetailsController(cravingContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Userdetails
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Userdetail>>> GetUserdetails()
+        {
+            return await _context.Userdetails.ToListAsync();
+        }
+
+        // GET: api/Userdetails/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Userdetail>> GetUserdetail(string id)
+        {
+          
+
+           var userdetail = await _context.Userdetails.FindAsync(id);
+                if (userdetail == null)
+                {
+                    return NotFound();
+                }
+
+                return userdetail;
+
+           
+        }
+
+        // PUT: api/Userdetails/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutUserdetail(string id,Userdetail userdetail)
+        {
+
+            
+
+            try
+            {
+                _context.Entry(userdetail).State = EntityState.Modified;
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!UserdetailExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut()]
+        public async Task<IActionResult> PutUserdetail(Userdetail userdetail)
+        {
+           
+
+            _context.Entry(userdetail).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!UserdetailExists(userdetail.Userid))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Userdetails
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Userdetail>> PostUserdetail(Userdetail userdetail)
+        {
+            _context.Userdetails.Add(userdetail);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetUserdetail", new { id = userdetail.Userid }, userdetail);
+        }
+
+        // DELETE: api/Userdetails/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteUserdetail(string id)
+        {
+            var userdetail = await _context.Userdetails.FindAsync(id);
+            if (userdetail == null)
+            {
+                return NotFound();
+            }
+
+            _context.Userdetails.Remove(userdetail);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool UserdetailExists(string id)
+        {
+            return _context.Userdetails.Any(e => e.Userid == id);
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,62 @@
+using CravingAPI.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CravingAPI
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<cravingContext>(options => options.UseSqlServer(Configuration.GetConnectionString("myconnection")));
+
+            services.AddCors(Options => {
+                Options.AddPolicy(name: "*",
+                     builder =>
+                     {
+                         builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+
+                     });
+            });
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseCors("*");
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
