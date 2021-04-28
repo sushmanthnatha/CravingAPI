f8e432e2cb1ab75dd082c267027909daef548cbf
Sushmanth Natha <sushmanthnatha@gmail.com>
2021-04-28 13:52:34 +0530

    added items controller and added model classes for : cart,item,order

---
+++ Models/Item.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace CravingAPI.Models
+{
+    public partial class Item
+    {
+        public Item()
+        {
+            Carts = new HashSet<Cart>();
+        }
+
+        public int Itemid { get; set; }
+        public int Quantity { get; set; }
+        public string Itemname { get; set; }
+        public string Category { get; set; }
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+        public bool Veg { get; set; }
+        public string Image { get; set; }
+        public double? Ratings { get; set; }
+
+        public virtual ICollection<Cart> Carts { get; set; }
+    }
+}
--- Models/cravingContext.cs
+++ Models/cravingContext.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata;
+using CravingAPI.Models;
 
 #nullable disable
 
@@ -95,5 +96,7 @@ namespace CravingAPI.Models
         }
 
         partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+
+        public DbSet<CravingAPI.Models.Item> Item { get; set; }
     }
 }
